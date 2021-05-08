ae5e59fd974bb76cd83b0719dbcec943b88e252e
vasco <vastur@gmail.com>
2021-05-09 00:39:44 +0100

    Consume root object from global THREE if available

--- src/three-globe.js
+++ src/three-globe.js
@@ -1,5 +1,7 @@
-import { Group as ThreeGroup } from 'three';
+import { Group } from 'three';
+const three = window.THREE ? window.THREE : { Group }; // Prefer consumption from global THREE, if exists
+
 import Globe from './globe-kapsule.js';
 import fromKapsule from './utils/kapsule-class.js';
 
-export default fromKapsule(Globe, ThreeGroup, true);
+export default fromKapsule(Globe, three.Group, true);
